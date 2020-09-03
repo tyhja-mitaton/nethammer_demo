ece528a8098078bcf8b78c221751c646893b2d59
Chagmar <abs.romik@gmail.com>
2020-09-04 00:09:03 +0500

    Страница ошибки

---
+++ frontend/web/html/error.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <?php include 'head.php'; ?>
+        <title>Контакты</title>
+    </head>
+
+    <body>
+       
+        <?php include 'header.php'; ?>
+
+        <div class="contact-page">
+            
+            <div class="container">
+                <h1 class="page-title">Страница не найдена</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="html/index.php">Главная</a></li>
+                        <li class="breadcrumb-item active">Страница не найдена</li>
+                    </ol>
+                </nav>
+                
+                <div class="text-center my-5">
+                    <h1>Ошибка 404</h1>
+                    <p>Что-то пошло не так.. </p>
+                    <p><a href="html/index.php" class="btn btn-blue-o d-inline-flex mt-3">Вернуться на главную</a></p>
+                </div>
+            </div>
+            
+        </div>
+        
+        <?php include 'footer.php'; ?>
+            
+    </body>
+</html>
